4c030ad952909c24446549b7da232b72f6dd6283
faridguluzade <tu5mhbxzt.code.edu.az>
2023-10-08 15:46:43 -0800

    fetched restaurants by city name & fetched data for side navbar

--- app/search/components/RestaurantCard.tsx
+++ app/search/components/RestaurantCard.tsx
@@ -1,28 +1,42 @@
+import { Cuisine, PRICE, Location } from "@prisma/client";
 import Link from "next/link";
+import Price from "../../components/Price";
 
-export default function RestaurantCard() {
+interface Restaurant {
+  id: number;
+  name: string;
+  main_image: string;
+  slug: string;
+  price: PRICE;
+  cuisine: Cuisine;
+  location: Location;
+}
+
+export default function RestaurantCard({
+  restaurant,
+}: {
+  restaurant: Restaurant;
+}) {
+  const { name, main_image, slug, price, cuisine, location } = restaurant;
+
   return (
-    <div className="border-b flex pb-5">
-      <img
-        src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
-        alt=""
-        className="w-44 rounded"
-      />
+    <div className="border-b flex pb-5 ml-4">
+      <img src={main_image} alt="" className="w-44 rounded" />
       <div className="pl-5">
-        <h2 className="text-3xl">Aiāna Restaurant Collective</h2>
+        <h2 className="text-3xl">{name}</h2>
         <div className="flex items-start">
           <div className="flex mb-2">*****</div>
           <p className="ml-2 text-sm">Awesome</p>
         </div>
         <div className="mb-9">
           <div className="font-light flex text-reg">
-            <p className="mr-4">$$$</p>
-            <p className="mr-4">Mexican</p>
-            <p className="mr-4">Ottawa</p>
+            <Price price={price} />
+            <p className="mr-4 capitalize">{cuisine.name}</p>
+            <p className="mr-4 capitalize">{location.name}</p>
           </div>
         </div>
         <div className="text-red-600">
-          <Link href="/restaurant/milesstones">View more information</Link>
+          <Link href={`/restaurant/${slug}`}>View more information</Link>
         </div>
       </div>
     </div>
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,21 +1,70 @@
-"use client";
-
-import { useSearchParams } from "next/navigation";
 import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
 import SearchSideBar from "./components/SearchSideBar";
 
-export default function Search() {
-  const searchParams = useSearchParams();
+import { PRICE, PrismaClient } from "@prisma/client";
 
-  console.log(searchParams.get("city"));
+const prisma = new PrismaClient();
+
+const fetchRestaurantsByCity = (city: string | undefined) => {
+  const select = {
+    id: true,
+    name: true,
+    main_image: true,
+    slug: true,
+    price: true,
+    cuisine: true,
+    location: true,
+  };
+
+  if (!city) return prisma.restaurant.findMany({ select });
+
+  return prisma.restaurant.findMany({
+    where: {
+      location: {
+        name: {
+          equals: city.toLowerCase().trim(),
+        },
+      },
+    },
+    select,
+  });
+};
+
+const fetchLocation = () => {
+  return prisma.location.findMany();
+};
+
+const fetchCuisine = () => {
+  return prisma.cuisine.findMany();
+};
+
+export default async function Search({
+  searchParams,
+}: {
+  searchParams: { city?: string; cuisine?: string; price?: PRICE };
+}) {
+  const restaurants = await fetchRestaurantsByCity(searchParams.city);
+  const location = await fetchLocation();
+  const cuisine = await fetchCuisine();
+
   return (
     <>
       <Header />
       <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-        <SearchSideBar />
+        <SearchSideBar
+          cuisines={cuisine}
+          locations={location}
+          searchParams={searchParams}
+        />
         <div className="w-5/6">
-          <RestaurantCard />
+          {restaurants.length ? (
+            restaurants.map((restaurant) => (
+              <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+            ))
+          ) : (
+            <p>Sorry, we found no restaurants in this area</p>
+          )}
         </div>
       </div>
     </>
