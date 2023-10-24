9f01d873564f3f7dae37ade00b33194a2821aacb
faridguluzade <tu5mhbxzt.code.edu.az>
2023-10-24 14:01:38 -0800

    Validating the user's input & Validating That the User's Doesn't Already Have an Account

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,12 +1,74 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import validator from "validator";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
+    const { firstName, lastName, email, city, phone, password } = req.body;
+    const errors: string[] = [];
+
+    const validationSchema = [
+      {
+        valid: validator.isLength(firstName, {
+          min: 1,
+          max: 10,
+        }),
+        errorMessage: "FirstName is not valid",
+      },
+      {
+        valid: validator.isLength(lastName, {
+          min: 1,
+          max: 10,
+        }),
+        errorMessage: "LastName is not valid",
+      },
+      {
+        valid: validator.isEmail(email),
+        errorMessage: "Email is not valid",
+      },
+      {
+        valid: validator.isLength(city, {
+          min: 1,
+        }),
+        errorMessage: "City is not valid",
+      },
+      {
+        valid: validator.isMobilePhone(phone),
+        errorMessage: "Phone is not valid",
+      },
+      {
+        valid: validator.isStrongPassword(password),
+        errorMessage: "Password is not strong enough",
+      },
+    ];
+
+    validationSchema.forEach((check) => {
+      if (!check.valid) {
+        errors.push(check.errorMessage);
+      }
+    });
+
+    if (errors.length) {
+      return res.status(400).json({ errorMessage: errors[0] });
+    }
+
+    const userWithEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userWithEmail) {
+      return res.status(400).json({ errorMessage: "Email already in use!" });
+    }
+
     res.status(200).json({
-      hello: "there",
+      hello: "body",
     });
   }
 }
