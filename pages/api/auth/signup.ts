6ca88764fc7767e3d2c8be30bf52b57e53708ba7
faridguluzade <tu5mhbxzt.code.edu.az>
2023-10-25 01:09:38 -0800

    Hashing password & creating user

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import validator from "validator";
 import { PrismaClient } from "@prisma/client";
+import validator from "validator";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
@@ -67,8 +68,21 @@ export default async function handler(
       return res.status(400).json({ errorMessage: "Email already in use!" });
     }
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        first_name: firstName,
+        last_name: lastName,
+        password: hashedPassword,
+        email,
+        city,
+        phone,
+      },
+    });
+
     res.status(200).json({
-      hello: "body",
+      hello: user,
     });
   }
 }
