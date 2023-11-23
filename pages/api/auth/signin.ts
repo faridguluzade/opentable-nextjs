b3799ccb9bb7fde5c64bf0f99fac2d19a2b19f1d
faridguluzade <tu5mhbxzt.code.edu.az>
2023-11-22 19:42:10 -0800

    Creating and Sending a JSON Web Token &  Implementing the Sign in Endpoint

--- pages/api/auth/signin.ts
+++ pages/api/auth/signin.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
+
 import { PrismaClient } from "@prisma/client";
 import validator from "validator";
 import bcrypt from "bcrypt";
@@ -11,41 +12,19 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
-    const { firstName, lastName, email, city, phone, password } = req.body;
     const errors: string[] = [];
+    const { email, password } = req.body;
 
     const validationSchema = [
-      {
-        valid: validator.isLength(firstName, {
-          min: 1,
-          max: 10,
-        }),
-        errorMessage: "FirstName is not valid",
-      },
-      {
-        valid: validator.isLength(lastName, {
-          min: 1,
-          max: 10,
-        }),
-        errorMessage: "LastName is not valid",
-      },
       {
         valid: validator.isEmail(email),
         errorMessage: "Email is not valid",
       },
       {
-        valid: validator.isLength(city, {
+        valid: validator.isLength(password, {
           min: 1,
         }),
-        errorMessage: "City is not valid",
-      },
-      {
-        valid: validator.isMobilePhone(phone),
-        errorMessage: "Phone is not valid",
-      },
-      {
-        valid: validator.isStrongPassword(password),
-        errorMessage: "Password is not strong enough",
+        errorMessage: "Password is not valid",
       },
     ];
 
@@ -65,29 +44,26 @@ export default async function handler(
       },
     });
 
-    if (userWithEmail) {
-      return res.status(400).json({ errorMessage: "Email already in use!" });
+    if (!userWithEmail) {
+      return res
+        .status(401)
+        .json({ errorMessage: "Email or password is not valid!" });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const isMatch = bcrypt.compare(password, userWithEmail.password);
 
-    const user = await prisma.user.create({
-      data: {
-        first_name: firstName,
-        last_name: lastName,
-        password: hashedPassword,
-        email,
-        city,
-        phone,
-      },
-    });
+    if (!isMatch) {
+      return res
+        .status(401)
+        .json({ errorMessage: "Email or password is not valid!" });
+    }
 
     const alg = "HS256";
 
     const secret = new TextEncoder().encode(process.env.JWT_SECRET);
 
     const token = await new jose.SignJWT({
-      email: user.email,
+      email: userWithEmail.email,
     })
       .setProtectedHeader({ alg })
       .setExpirationTime("24h")
@@ -97,4 +73,6 @@ export default async function handler(
       token,
     });
   }
+
+  return res.status(404).json("Unknown endpoint");
 }
