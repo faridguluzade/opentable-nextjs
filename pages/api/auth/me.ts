f7df12c80b954778a4ae39319f9c9c97ec643534
faridguluzade <tu5mhbxzt.code.edu.az>
2023-11-22 23:23:02 -0800

    adding middleware

--- pages/api/auth/me.ts
+++ pages/api/auth/me.ts
@@ -10,24 +10,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const bearerToken = req.headers["authorization"] as string;
-
-  if (!bearerToken) {
-    return res.status(401).json({ errorMessage: "Unauthorized request" });
-  }
-
   const token = bearerToken.split(" ")[1];
-
-  if (!token) {
-    return res.status(401).json({ errorMessage: "Unauthorized request" });
-  }
-
-  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-
-  try {
-    await jose.jwtVerify(token, secret);
-  } catch (error) {
-    return res.status(401).json({ errorMessage: "Unauthorized request" });
-  }
 
   const payload = jwt.decode(token) as { email: string };
 
