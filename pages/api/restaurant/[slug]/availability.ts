de41baf7cf018e237fe44f5d0abeb0c502845f9b
faridguluzade <tu5mhbxzt.code.edu.az>
2023-11-29 00:05:40 -0800

    determining the search times & fetching the bookings

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -1,5 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+import { PrismaClient } from "@prisma/client";
+
+import { times } from "../../../../data/times";
+
+const prisma = new PrismaClient();
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -14,7 +20,34 @@ export default async function handler(
   if (!day || !time || !partySize) {
     return res.status(400).json({ errorMessage: "Invalid data provide" });
   }
-  return res.json({ slug, day, time, partySize });
+
+  const searchTimes = times.find((t) => {
+    return t.time === time;
+  })?.searchTimes;
+
+  if (!searchTimes) {
+    return res.status(400).json({ errorMessage: "Invalid data provide" });
+  }
+
+  console.log(searchTimes);
+
+  const bookings = await prisma.booking.findMany({
+    where: {
+      booking_time: {
+        gte: new Date(`${day}T${searchTimes[0]}`),
+        lte: new Date(`${day}T${searchTimes[searchTimes.length - 1]}`),
+      },
+    },
+    select: {
+      number_of_people: true,
+      booking_time: true,
+      tables: true,
+    },
+  });
+
+  console.log(bookings);
+
+  return res.json({ searchTimes, bookings });
 }
 
-// http://localhost:3000/api/restaurant/ramakrishna-indian-restaurant-ottawa/availability?day=20&time=salam&partySize=qocu
+// http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/availability?day=2023-11-28&time=14:00:00.000Z&partySize=4
