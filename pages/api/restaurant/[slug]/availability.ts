366a9359f4b5a4c24bc72f05057639f17a30f5df
faridguluzade <tu5mhbxzt.code.edu.az>
2023-11-29 00:37:53 -0800

    compressing the booking & fetcing the restaurants

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -29,8 +29,6 @@ export default async function handler(
     return res.status(400).json({ errorMessage: "Invalid data provide" });
   }
 
-  console.log(searchTimes);
-
   const bookings = await prisma.booking.findMany({
     where: {
       booking_time: {
@@ -45,9 +43,34 @@ export default async function handler(
     },
   });
 
-  console.log(bookings);
+  const bookingTablesObj: { [key: string]: { [key: number]: true } } = {};
 
-  return res.json({ searchTimes, bookings });
+  bookings.forEach((booking) => {
+    bookingTablesObj[booking.booking_time.toISOString()] =
+      booking.tables.reduce((acc, cur) => {
+        return {
+          ...acc,
+          [cur.table_id]: true,
+        };
+      }, {});
+  });
+
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      tables: true,
+    },
+  });
+
+  if (!restaurant) {
+    return res.status(400).json({ errorMessage: "Invalid data provide" });
+  }
+
+  const tables = restaurant.tables;
+
+  return res.json({ searchTimes, bookings, bookingTablesObj, tables });
 }
 
 // http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/availability?day=2023-11-28&time=14:00:00.000Z&partySize=4
