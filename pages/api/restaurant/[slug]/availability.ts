14c3b42d3fb644ed8d0a2438c16d686eb487550b
faridguluzade <tu5mhbxzt.code.edu.az>
2023-11-29 23:14:52 -0800

    Addign reservation endpoint

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -118,4 +118,4 @@ export default async function handler(
   return res.json(availabilities);
 }
 
-// http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/availability?day=2023-11-28&time=14:00:00.000Z&partySize=4
+// http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/reserve?day=2023-11-28&time=14:00:00.000Z&partySize=4
